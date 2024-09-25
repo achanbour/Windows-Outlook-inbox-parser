a13c5e583a029088b5480b4ed523cea4659b532d
Anastasia Chanbour <69864747+achanbour@users.noreply.github.com>
2024-09-25 10:49:50 +0100

    Update email_parser.py

--- email_parser.py
+++ email_parser.py
@@ -6,31 +6,13 @@ import ctypes
 import traceback
 
 """
-Create a mapping between the full month name and month keyword
-"""
-months_map = {
-    "January": "Jan",
-    "February": "Feb",
-    "March": "Mar",
-    "April": "Apr",
-    "May": "May",
-    "June": "Jun",
-    "July": "Jul",
-    "August": "Aug",
-    "September": "Sep",
-    "October": "Oct",
-    "November": "Nov",
-    "December": "Dec",
-}
-
-"""
 Read the mailing list as a pandas dataframe
 """
 mailing_list = "mailing_list.xlsx"
 
 mailing_list_df = pd.read_excel(mailing_list)
 
-# Fill the any NA values in the mailing list dataframe
+# Fill the any NA values in the mailing list
 mailing_list_df = mailing_list_df.fillna(method="ffill", axis=0)
 
 # Define custom Exception raised when Outlook is shut
@@ -62,18 +44,18 @@ def monitor_outlook_inbox():
 
             outlook = win32.Dispatch("Outlook.Application")  # Outlook app
             namespace = outlook.GetNameSpace("MAPI")  # Outlook API
-            folder = namespace.Folders.Item("myemail@test.com")
+            folder = namespace.Folders.Item("example@example.com") # Outlook email folder to be monitored
             inbox = folder.Folders.Item("Inbox")  # Main inbox
             messages = inbox.Items  # Get the inbox items
 
             for message in messages:
 
                 """
-                Step I: Iterate over unread emails with subject containing the defined keyword: test
+                Step I: Iterate over unread emails with subject containing the defined keyword: Appollo
                 """
-                if (message.Unread == True) and ("test" in message.Subject):
+                if (message.Unread == True) and ("Appollo" in message.Subject):
 
-                    subject = message.Subject
+                    subject = message.Subject # Retrieve the subject of the email
                     print("Email found!")
                     print(f"Subject: {subject}")
 
@@ -82,22 +64,20 @@ def monitor_outlook_inbox():
                     else:  # email type SMTP
                         sender = message.SenderEmailAddress
 
-                    date_and_time = message.SentOn.strftime("%Y-%m-%d--%H-%M")
+                    date_and_time = message.SentOn.strftime("%Y-%m-%d--%H-%M") # Retrieve the date and time the email was sent
                     print(f"Date & time received: {date_and_time}")
                     print(f"Sender: {sender}")
 
                     """
                     Step II: Archive the email and any attachments in the defined archive location
                     """
-                    # Create year folder in the Emails Received directory
+                    # Define the path to the archiving location
                     archive_location = "C:\Users\user\Desktop"
                   
-                    # Eliminate any special characters in the name
-                    new_subject = str(date_and_time) + " " + \
-                        subject.replace("RE:", "")
-                  
-                    name = new_subject + ".msg"
-                    message.SaveAs(archive_location + "\\" + name)
+                    # Eliminate any special characters in the email subject
+                    new_subject = str(date_and_time) + " " + subject.replace("RE:", "")
+                    filename = new_subject + ".msg"
+                    message.SaveAs(os.path.join(archive_location,filename))
                     message.unread = False  # set the email as Read
 
                     """
@@ -105,22 +85,21 @@ def monitor_outlook_inbox():
                     """
 
                     # Get the current timestamp and include it in the attachment file name when saving
-
                     today = datetime.today().strftime("%Y-%m-%d--%H-%M")
-
+                    
                     for att in message.Attachments:
-                        file = today + att.FileName
-                        att.SaveAsFile(archive_location + "/" + file)
+                        attachement_filename = today + att.FileName
+                        att.SaveAsFile(os.path.join(archive_location, attachement_filename))
 
                     """
-                    Step IV: Send a notification informing that an email has been detected only after the archive is complete
+                    Step IV: Send a notification informing that an email has been detected. This step is executed only after the email has been successfully archived
                     """
-                    recipients = "recipient@test.com"
+                    recipients = "recipient@example.com"
                     notification = outlook.CreateItem(0)
                     notification.Subject = "Notification subject"
-                    notification.Body = "Hello,\n\nThis is an automated notification informing you that an email with \"test\" in subject has been detected. This email has been successfully archived in the defined target location"
+                    notification.Body = "Hello,\n\nThis is an automated notification informing you that an email with \"Appollo\" in subject title has been detected. This email has been successfully archived in the defined target folder"
                     notification.To = recipients
-                    notification.SentOnBehalfOfName = "myemail@test.com"
+                    notification.SentOnBehalfOfName = "example@example.com"
                     notification.Send()
                     print("Notification successfully sent!")
                     print("\n")
